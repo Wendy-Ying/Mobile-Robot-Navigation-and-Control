fe3f64665e79f782c9e1ce57ef157f83f4776c15
Wendy-Ying <143325815+Wendy-Ying@users.noreply.github.com>
2024-11-13 16:08:42 +0800

    Add files via upload

--- lab4/scripts/scan.py
+++ lab4/scripts/scan.py
@@ -6,31 +6,44 @@ import numpy as np
 
 def callback(scan):
     distance_list = []
-    for index in range(1,len(scan.ranges)):
+    for index in range(0,len(scan.ranges)):
         distance = scan.ranges[index]
         distance_list.append(distance)
-    angle = calculate_target_angle(distance_list)
-    if len(angle) > 0:
-        print(angle[0])
+    dis, angle = calculate_target_angle(distance_list)
+    print(dis, angle)
         
 
-def calculate_target_angle(radar_distances, threshold=0.5, min_distance=1, max_distance=1.5):
-    
-    # compute gradient
+def calculate_target_angle(radar_distances, threshold=0.5):
     distance_gradient = np.diff(radar_distances)
-    # find edge
     edges = np.where(np.abs(distance_gradient) > threshold)[0]
     if len(edges) == 0:
-        return None
+        return None, None
     
-    # limit the distance
-    pole_angle = []
+    pole_angles = []
+    pole_distances = []
+
+    # 定义偏移量和距离差，用于简化循环逻辑
+    offset_distance_pairs = [
+        (0, 0.5),
+        (1, 0.5),
+        (-1, 0.5)
+    ]
+
     for edge in edges:
-        if (min_distance <= radar_distances[edge % 360] <= max_distance and
-            radar_distances[(edge + 357) % 360] >= min_distance and
-            radar_distances[(edge + 3) % 360] >= min_distance):
-            pole_angle.append(edge)
-    return pole_angle
+        for offset, distance_diff in offset_distance_pairs:
+            dis1 = radar_distances[(edge + 3 + offset) % 360]
+            dis2 = radar_distances[(edge + offset) % 360]
+            dis3 = radar_distances[(edge + 357 + offset) % 360]
+            
+            # 检查条件并存储满足条件的角度和距离
+            if (dis1 - dis2 > distance_diff) and (dis3 - dis2 > distance_diff) and dis2 > 0:
+                pole_angles.append((edge + offset) % 360)
+                pole_distances.append(dis2)
+                break  # 一旦找到满足条件的就跳出内层循环
+
+    if pole_angles:
+        return pole_distances, pole_angles
+    return None, None
     
 
 def listener():
