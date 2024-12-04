9d47a8a8ddecabfdafc48fcf29fc028f1d881b47
Yiwen Ying <143325815+Wendy-Ying@users.noreply.github.com>
2024-12-04 16:15:40 +0800

    Add files via upload

--- lab6/scripts/send_goal.py
+++ lab6/scripts/send_goal.py
@@ -4,18 +4,19 @@
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import Bool
 
-def movebase_client():
-
-    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+# This function will send the move goal with provided parameters
+def movebase_client(x, y, w):
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
 
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = -1.1
-    goal.target_pose.pose.position.y = 0.4
-    goal.target_pose.pose.orientation.w = 1.0
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.orientation.w = w
 
     client.send_goal(goal)
     wait = client.wait_for_result()
@@ -25,11 +26,52 @@ def movebase_client():
     else:
         return client.get_result()
 
+# Publisher to notify when the robot reaches the goal
+def talker():
+    rospy.init_node('find_pole_talker', anonymous=True)
+    pub = rospy.Publisher('find_pole', Bool, queue_size=10)
+    rate = rospy.Rate(1)  # 1Hz
+
+    while not rospy.is_shutdown():
+        msg = Bool(data=True)
+        rospy.loginfo(f"Publishing to 'find_pole' topic: {msg.data}")
+        pub.publish(msg)
+        rate.sleep()
+
+# Callback function for the 'goto' topic
+def goto_callback(msg):
+    global goto_counter
+
+    if msg.data:  # If True, change the target position
+        if goto_counter == 0:
+            rospy.loginfo("Moving to first position (-1.1, 0.4, 1.0)")
+            movebase_client(-1.1, 0.4, 1.0)
+        elif goto_counter == 1:
+            rospy.loginfo("Moving to second position (1, 1, 1)")
+            movebase_client(1, 1, 1)
+        elif goto_counter == 2:
+            rospy.loginfo("Moving to third position (2, 2, 0.5)")
+            movebase_client(2, 2, 0.5)
+
+        goto_counter += 1
+
+def movebase_node():
+    global goto_counter
+    goto_counter = 0  # Initialize the counter to control the sequence of movements
+
+    # Initialize the ROS node
+    rospy.init_node('movebase_client_py')
+
+    # Subscribe to the "goto" topic
+    rospy.Subscriber('goto', Bool, goto_callback)
+
+    # Start the ROS loop
+    rospy.spin()
+
 if __name__ == '__main__':
     try:
-        rospy.init_node('movebase_client_py')
-        result = movebase_client()
-        if result:
-            rospy.loginfo("Goal execution done!")
+        # Call the movebase_node to handle the movement logic
+        movebase_node()
+
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
